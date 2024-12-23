03993534b45f86c771f4efee6158b172c03decaf
wednesday0011 <sanhari121@gmail.com>
2024-12-23 20:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# main.py
 import os
-from keep_alive import keep_alive
 from bot import run_bot
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
-
 if __name__ == "__main__":
-    keep_alive()
-    run_bot(BOT_TOKEN)
+    TOKEN = os.getenv('BOT_TOKEN')
+    if TOKEN:
+        run_bot(TOKEN)
+    else:
+        print("No token found!")
