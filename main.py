b387d046530ad0d045a1209bd6fdfaf41c3e5270
wednesday0011 <sanhari121@gmail.com>
2024-12-23 19:40:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# main.py
+import os
+from keep_alive import keep_alive
+from bot import run_bot
 
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+if __name__ == "__main__":
+    keep_alive()
+    run_bot(BOT_TOKEN)
